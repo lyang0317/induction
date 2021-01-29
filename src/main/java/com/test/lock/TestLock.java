3e8d9750d9710e3d10ee39b1c2dc25db0a0ea50b
lujy7 <lujy7@lenovo.com>
2021-01-29 11:07:59 +0800

    1

--- src/main/java/com/test/lock/TestLock.java
+++ src/main/java/com/test/lock/TestLock.java
@@ -1,6 +1,7 @@
 package com.test.lock;
 
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * FileName: TestLock
@@ -12,7 +13,7 @@ public class TestLock {
 
 
     public static void main(String[] args) {
-        testInterrupt();
+        testReadWriteLock();
     }
 
     private static void testLock() {
@@ -22,7 +23,7 @@ public class TestLock {
                 try {
                     reentrantLock.lock();
                     System.out.println("thread 1 lock");
-                    Thread.sleep(1000000);
+                    Thread.sleep(10000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
@@ -52,11 +53,11 @@ public class TestLock {
             }
         }).start();
         try {
-            Thread.sleep(10000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("sleep 10 seconds between 2 and 3");
+        System.out.println("sleep 1 seconds between 2 and 3");
         new Thread(new Runnable() {
             public void run() {
                 try {
@@ -96,4 +97,64 @@ public class TestLock {
         thread.interrupt();//作用是：在线程阻塞时抛出一个中断信号，这样线程就得以退出阻塞的状态
     }
 
+    private static void testReadWriteLock() {
+        ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    reentrantReadWriteLock.writeLock().lock();
+                    System.out.println("thread 1 lock");
+                    Thread.sleep(5000);
+                    reentrantReadWriteLock.readLock().lock();
+                    Thread.sleep(500000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    reentrantReadWriteLock.readLock().unlock();
+                    System.out.println("thread 1 unlock");
+                }
+            }
+        }).start();
+        /*try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("sleep 5 seconds between 1 and 2");
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    reentrantReadWriteLock.readLock().lock();
+                    System.out.println("thread 2 lock");
+                    Thread.sleep(1000000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    reentrantReadWriteLock.readLock().unlock();
+                    System.out.println("thread 2 unlock");
+                }
+            }
+        }).start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("sleep 5 seconds between 2 and 3");
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    reentrantReadWriteLock.readLock().lock();
+                    System.out.println("thread 3 lock");
+                    Thread.sleep(1000000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    reentrantReadWriteLock.readLock().unlock();
+                    System.out.println("thread 3 unlock");
+                }
+            }
+        }).start();*/
+        System.out.println("main end");
+    }
 }
