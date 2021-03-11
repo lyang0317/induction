ec0e7e8a5845b7e7dc0bc22bb7de1cb44e0510c1
lujy7 <lujy7@lenovo.com>
2021-03-11 10:45:05 +0800

    1

--- src/main/java/com/test/WorkTest.java
+++ src/main/java/com/test/WorkTest.java
@@ -7,10 +7,21 @@ public class WorkTest {
     private static int STEP = 0;
 
     public static void main(String[] args) {
-        System.out.println("------ step list ------");
+        long nextIndex = 116864003;
+        long itemType = 4;
+        long counterAndTypeCode = nextIndex << 15 | itemType;
+        System.out.println(counterAndTypeCode);
+        long upperWithoutBit43 = (counterAndTypeCode & 0xFFFFF80000000000L) << 1;
+        System.out.println(upperWithoutBit43);
+        long lowerWithoutBit43 = counterAndTypeCode & 0x7FFFFFFFFFFL;
+        System.out.println(lowerWithoutBit43);
+        System.out.println(upperWithoutBit43 | 0x80000000000L | lowerWithoutBit43);
+
+
+        /*System.out.println("------ step list ------");
         getStep(3);
         System.out.println("------- step num -----");
-        System.out.println(STEP);
+        System.out.println(STEP);*/
     }
 
     private static void getStep(int i) {
@@ -22,5 +33,6 @@ public class WorkTest {
         STEP ++;
         getStep(i - i1);
     }
+
 
 }
