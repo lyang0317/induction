1c006c401ef53cb3bf8cd9609b607bd699ecd031
lujy7 <lujy7@lenovo.com>
2023-06-21 15:38:24 +0800

    8

---
+++ src/main/java/com/test/completionstage/ModeA.java
@@ -0,0 +1,28 @@
+package com.test.completionstage;
+
+public class ModeA {
+
+    private int a;
+    private String b;
+
+    public ModeA(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
---
+++ src/main/java/com/test/completionstage/ModeB.java
@@ -0,0 +1,28 @@
+package com.test.completionstage;
+
+public class ModeB {
+
+    private String x;
+    private String y;
+
+    public ModeB(String x, String y) {
+        this.x = y;
+        this.x = y;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+}
